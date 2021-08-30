10abf0e78ca20d0d79ba38a1aa31c4b88603755f
Ryel Banfield <ryelbanfield@gmail.com>
2021-08-30 16:13:42 -0400

    Start building country component

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,15 +1,22 @@
 import Favourite from './Favourite';
 
 /* eslint-disable react/prop-types */
-const Country = ({ country, isFavourited }) => {
-  console.log(isFavourited);
-
-  return (
-    <>
-      <h1>{country.name}</h1>
-      <Favourite country={country} isFavourited={isFavourited} />
-    </>
-  );
-};
+const Country = ({ country, isFavourited }) => (
+  <div className="country">
+    <h1>{country.name}</h1>
+    <Favourite country={country} isFavourited={isFavourited} />
+    <p>{`Capital: ${country.capital}`}</p>
+    <p>{`Population: ${Number(country.population).toLocaleString()}`}</p>
+    <p>{`Language: ${country.language}`}</p>
+    <p>{`Demonym: ${country.demonym}`}</p>
+    <p>{`Currency: ${country.currency}`}</p>
+    <div className="country-images">
+      <img src={country.flag} alt={`Flag of ${country.name}`} />
+      {country && country.images.map((image) => (
+        <img src={image.url} alt={country.capital} key={image} />
+      ))}
+    </div>
+  </div>
+);
 
 export default Country;
