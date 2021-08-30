0d57c3fc9c680bb6f6a7dd70ac2741f94c057164
Ryel Banfield <ryelbanfield@gmail.com>
2021-08-30 12:23:37 -0400

    Remove position fixed from nav bar

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const SearchResults = ({ results }) => {
   const [foundResults, setFoundResults] = useState(null);
@@ -17,15 +18,17 @@ const SearchResults = ({ results }) => {
       {foundResults && (
       <div className="search-results">
         {results.map((result) => (
-          <div className="search-result" key={result.id}>
-            <img src={result.images[0].url} alt={result.name} />
-            <div className="overlay">
-              <div className="overlay-content">
-                <h2>{result.name}</h2>
-                <h3>{result.capital}</h3>
+          <Link to={`/countries/${result.name}`} key={result.id}>
+            <div className="search-result" key={result.id}>
+              <img src={result.images[0].url} alt={result.name} />
+              <div className="overlay">
+                <div className="overlay-content">
+                  <h2>{result.name}</h2>
+                  <h3>{result.capital}</h3>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
       )}
