4124208e9039e74523ea6b351ce1670d9563fc70
Ryel Banfield <ryelbanfield@gmail.com>
2021-09-01 06:06:58 -0400

    Add new loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,22 @@
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
 const Loading = () => (
-  <div>
-    <p>Loading...</p>
+  <div className="loading">
+    <p>
+      LOADING
+    </p>
+    <Loader
+      type="MutatingDots"
+      color="#000"
+      secondaryColor="#000"
+      height={100}
+      width={100}
+    />
+    <p>
+      PLEASE WAIT
+      SERVER MAY BE ASLEEP
+    </p>
   </div>
 );
 
